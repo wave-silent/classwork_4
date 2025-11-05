c39ea0d639c757b1dee06eb41372339f2ad35c5d
wave-silent <danillogutov.74@mail.ru>
2025-11-05 16:13:12 +0500

    Add files via upload

---
+++ Example.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classwork_4
+{
+    internal class Example
+    {
+        private int _x;
+        private int _y;
+
+
+        public Example()
+        {
+            _x = 0;
+            _y = 0;
+        }
+
+        public Example(int x, int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public void print()
+        {
+            Console.WriteLine(_x + " " + _y);
+
+        }
+        // Унарные операции 
+        public static Example operator ++(Example example)
+        {
+            // так нельзя делать 
+            //example._x++;
+
+            Example ex = new Example(++example._x, ++example._y);
+
+            return ex;
+
+            //return new Example(++example._x, ++example._y); 
+        }
+        public static Example operator --(Example example)
+        {
+            return new Example(--example._x, --example._y); 
+        }
+
+        // бинарные операции
+        public static Example operator +(Example ex1, Example ex2)
+        {
+            return new Example(ex1._x + ex2._x, ex1._y + ex2._y);
+        }
+
+        public static Example operator -(Example ex1, Example ex2)
+        {
+            return new Example(ex1._x - ex2._x, ex1._y - ex2._y);
+        }
+        // Леосторонняя
+        public static Example operator +(Example ex1, int n)
+        {
+            return new Example(ex1._x + n, ex1._y + n);
+        }
+        // Правосторонняя
+        public static Example operator +(int n, Example ex1)
+        {
+            return ex1 + n;
+        }
+        public static bool operator >(Example ex1, Example ex2)
+        {
+            return (ex1._x + ex1._y) > (ex2._x + ex2._y);
+        }
+
+        public static bool operator <(Example ex1, Example ex2)
+        {
+            return (ex1._x + ex1._y) < (ex2._x + ex2._y);
+        }
+
+        // Приведение типа 
+        // Явная операция - explicit. Неявная implicit
+        public static explicit operator int(Example e)
+        {
+            return e._x;
+        }
+
+        public static implicit operator bool(Example e)
+        {
+            return e._x == e._y;
+        }
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classwork_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Example ex1 = new Example(10, 20);
+            Example ex2 = new Example(20, 30);
+            Example ex3 = new Example();
+
+            ex3 = ++ex1;
+
+            ex3.print();
+
+            ex3 = --ex3;
+
+            ex3.print();
+
+            ex3 = ex1 + ex2;
+
+            ex3.print();
+
+            ex3 = ex1 - ex2;
+
+            ex3.print();
+
+            ex2 = ex1 + 10;
+
+            ex2.print();
+
+            ex2 = 10 + ex1;
+
+            ex2.print();
+
+            bool b = ex1 > ex2;
+
+            Console.WriteLine(b);
+            // явное приведение
+            int a = (int)ex2;
+
+            Console.WriteLine(a);
+
+            // неявное приведение
+            bool z = ex2;
+
+            Console.WriteLine(z);*/
+
+            List<int> list = new List<int>();
+
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+
+            // list.Remove(0) - удаляет конкретное значение
+
+            // list.RemoveAt() - удаляет по индексу 
+
+            list.Remove(0);
+            list.RemoveAt(1);
+
+            list[0] = 1;
+
+            //Возвращает кол-во списка/коллекции
+            int x = list.Count;
+
+            HashSet<int> set1 = new HashSet<int>();
+            set1.Add(1);
+            set1.Add(2);
+            set1.Add(3);
+            set1.Add(1); // 1 2 3
+
+            //set[0] - не можем обращаться 
+
+            HashSet<int> set2 = new HashSet<int>();
+            set2.Add(3);
+            set2.Add(4);
+            set2.Add(5); // 3 4 5
+
+            //set1.UnionWith(set2); // 1 2 3 4 5
+
+            //set1.IntersectWith(set2); // 3
+
+            set1.ExceptWith(set2); // 1 2 
+
+            // Вывод такой же как и у словарей
+   
+            // Словари 
+            Dictionary<int, string> keyValuePairs = new Dictionary<int, string>();
+
+            // Примеры на телефоне 
+
+        }
+    }
+}
